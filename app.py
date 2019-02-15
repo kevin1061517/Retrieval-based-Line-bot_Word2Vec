f35da43516ad2eb6ef09c7fefd8fa55866303940
kevin <kevin1061517@gmail.com>
2019-02-15 23:27:12 +0800

    add code

--- app.py
+++ app.py
@@ -2347,10 +2347,11 @@ def handle_msg_text(event):
             message
         )
     elif event.message.text.lower() == 'member':
+        print('----in')
         group_id = event.source.group_id
         member_ids_res = line_bot_api.get_group_member_ids(group_id)
-        print(member_ids_res.member_ids)
-        print(member_ids_res.next)
+#        print(member_ids_res.member_ids)
+#        print(member_ids_res.next)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=str(member_ids_res))
